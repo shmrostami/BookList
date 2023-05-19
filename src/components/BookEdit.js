cac8141c074d9c828d314832e492ea7c1440dbf9
Hossein Rostami <shm.rostami@gmail.com>
2023-05-19 10:16:03 +0330

    Initial

---
+++ src/components/BookEdit.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import useBooksContext from "../hooks/use-books-context";
+
+function BookEdit({ book, onEdit, onSubmit }) {
+    const [title, setTitle] = useState(book.title);
+    const { editBookById } = useBooksContext();
+
+    const handleChange = (e) => setTitle(e.target.value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        onSubmit();
+        editBookById(book.id, title);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input className="input" value={title} onChange={handleChange} />
+            <button className="button is-primary">Save</button>
+        </form>
+    );
+}
+
+export default BookEdit;
---
+++ src/components/BookShow.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+import useBooksContext from "../hooks/use-books-context";
+
+function BookShow({ book }) {
+    const [showEdit, setShowEdit] = useState(false);
+    const { deleteBookById } = useBooksContext();
+
+    const handleSubmit = () => {
+        setShowEdit(false);
+    };
+
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit book={book} onSubmit={handleSubmit} />;
+    }
+
+    return (
+        <div className="book-show">
+            <img
+                src={`https://picsum.photos/seed/${book.id}/300/200`}
+                alt="books"
+            />
+            <div>{content}</div>
+            <div className="actions">
+                <button className="edit" onClick={() => setShowEdit(!showEdit)}>
+                    Edit
+                </button>
+                <button
+                    className="delete"
+                    onClick={() => deleteBookById(book.id)}
+                >
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default BookShow;
